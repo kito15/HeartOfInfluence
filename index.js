c33c5e0eb871bdb154b437744937125858d5adad
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-12-26 15:25:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    const { firstname, lastname, email, selectedCampaign } = req.body;
+
+    // Log the received data
+    console.log('Received Data:');
+    console.log('Firstname:', firstname);
+    console.log('Lastname:', lastname);
+    console.log('Email:', email);
+    console.log('Selected Campaign:', selectedCampaign);
+
+    // Forward the data to Zapier
+    sendToZapier(req.body)
+        .then(() => {
+            res.status(200).send('Data received and forwarded to Zapier successfully.');
+        })
+        .catch((error) => {
+            console.error('Error forwarding data to Zapier:', error);
+            res.status(500).send('Internal Server Error.');
+        });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+function sendToZapier(data) {
+    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/15640277/3a0igbf/';
+
+    return axios.post(zapierWebhookUrl, data);
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
